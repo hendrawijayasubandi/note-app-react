5f37a573bc1acf4371b7e20affd466e06cf3c1da
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2023-04-04 11:36:41 +0700

    mod note item

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -6,9 +6,13 @@ import ArchiveButton from './ArchiveButton';
 function NoteItem({ title, body, createdAt, id, onDelete, onArchive }) {
     return (
         <div className="note-item">
-            <NoteItemBody title={title} createdAt={createdAt} body={body} />
-            <DeleteButton id={id} onDelete={onDelete} />
-            <ArchiveButton id={id} onArchive={onArchive} />
+            <div className="note-item__content">
+                <NoteItemBody title={title} createdAt={createdAt} body={body} />
+            </div>
+            <div className="note-item__action">
+                <DeleteButton id={id} onDelete={onDelete} />
+                <ArchiveButton id={id} onArchive={onArchive} />
+            </div>
         </div>
     );
 }
