e40c7acb6e11776331cc6ae664eb0ddabaae7fd3
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2023-10-25 22:13:18 +0700

    rename class to className

--- src/components/NoteHeader.jsx
+++ src/components/NoteHeader.jsx
@@ -5,7 +5,7 @@ function NoteHeader({ search, setQuery }) {
     return (
         <div className="note-app__header">
             <h1>Aplikasi Catatan</h1>
-            <div class="note-search">
+            <div className="note-search">
                 <NoteInput type="search" placeholder="Cari catatan ..." value={search} onChange={setQuery} />
             </div>
         </div>
