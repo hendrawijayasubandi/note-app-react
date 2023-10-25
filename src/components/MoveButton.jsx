a663e2becbd963f72ab719c9a272d8ec4853036b
hendrawijayasubandi <hendrawijayasubandi@gmail.com>
2023-10-25 23:05:27 +0700

    remove movebutton

--- src/components/MoveButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function MoveButton({ id, onMove }) {
-    return <button className='note-item__move-button' onClick={() => onMove(id)}>Pindahkan</button>
-}
-
-export default MoveButton;
